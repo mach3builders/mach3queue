e7aadd98832298922ff202f907916e770daa1b2e
Rewi <rewi@mach3builders.nl>
2024-02-08 14:07:18 +0100

    commands

--- src/Console/Console.php
+++ src/Console/Console.php
@@ -7,17 +7,21 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 
 class Console
 {
-    public function __construct(
-        private array $config,
-        private array $args
-    ) {
+    public function __construct(private array $config)
+    {
     }
 
-    public function handle(ArgvInput $input, ConsoleOutput $output)
+    public function handle(array $argv)
     {
-        match ($this->args[1]) {
-            'app:worker' => (new WorkerCommand($this->config))->execute($input, $output),
+        $params = $argv;
+        unset($params[1]);
+        $input = new ArgvInput($params);
+        $output = new ConsoleOutput;
+
+        match ($argv[1]) {
+            'install' => (new InstallCommand($this->config))->run($input, $output),
+            'worker' => (new WorkerCommand($this->config))->run($input, $output),
             default => null,
-        };     
+        };
     }
 }
--- src/Console/WorkerCommand.php
+++ src/Console/WorkerCommand.php
@@ -7,26 +7,31 @@ use Mach3queue\Worker\Worker;
 use Mach3queue\Worker\WorkerActions;
 use Mach3queue\Worker\WorkerOptions;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class WorkerCommand extends Command
 {
     public function __construct(private array $config)
     {
-        parent::__construct('Worker Command');
+        parent::__construct();
     }
 
-    public function execute(InputInterface $input, OutputInterface $output): int
+    protected function configure(): void
     {
-        $queue = new Queue;
-        $queue->setConnection([
-            'driver' => $this->config['db_driver'] ?? 'mysql',
-            'host' => $this->config['db_host'],
-            'database' => $this->config['db_name'],
-            'username' => $this->config['db_user'],
-            'password' => $this->config['db_pass'],
-        ]);
+        $this->setDefinition(
+            new InputDefinition([
+                new InputOption('stop-when-empty', 's'),
+                new InputOption('queue', 'q', InputOption::VALUE_REQUIRED),
+                new InputOption('timeout', 't', InputOption::VALUE_OPTIONAL),
+            ])
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
         $worker_params = $this->workerParams($input);
         $worker = new Worker(...$worker_params);
         $worker->run();
@@ -36,8 +41,18 @@ class WorkerCommand extends Command
 
     private function workerParams(InputInterface $input): array
     {
+        $queue_name = $input->getOption('queue') ?? Queue::DEFAULT_QUEUE;
+        $queue = new Queue;
+        $queue->setConnection([
+            'driver' => $this->config['db_driver'] ?? 'mysql',
+            'host' => $this->config['db_host'],
+            'database' => $this->config['db_name'],
+            'username' => $this->config['db_user'],
+            'password' => $this->config['db_pass'],
+        ]);
+
         return [
-            'queue' => $input->getOption('queue') ?? Queue::DEFAULT_QUEUE,
+            'queue' => $queue->on($queue_name),
             'timeout' => $input->getOption('timeout') ?? 60,
             'actions' => new WorkerActions,
             'options' => new WorkerOptions(
